2891ec53154144eeae5c072a2d967661b3887152
jparasha <jayant.parashar@capgemini.com>
2017-08-28 12:25:03 +0530

    Added more regions

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ exports.handler = function (event, context) {
 
 	//////////////////////////////////////////////////Function call for CLients of Each Region////////////////////////////////////////////
 
-	var regionNames = ['us-west-1', 'us-west-2', 'us-east-1', 'eu-west-1', 'eu-central-1', 'sa-east-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2'];
+	var regionNames = ['us-west-1', 'us-west-2', 'us-east-1', 'us-east-2', 'eu-west-1', 'eu-west-2', 'eu-central-1', 'sa-east-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2', 'ap-south-1', 'ca-central-1'];
 	regionNames.forEach(function (region) {
 		describeAllInstances(region);
 	});
